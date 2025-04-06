c6d12d0a6d98677484c7045c946f48459a5d370b
Jacob Asir <122958208+JacobAsir@users.noreply.github.com>
2025-04-06 23:48:19 +0530

    Add files via upload

---
+++ crew.py
@@ -0,0 +1,17 @@
+from crewai import Crew, Process
+from agents import blog_researcher, blog_writer
+from tasks import research_task, write_task
+
+crew = Crew(
+    agents=[blog_researcher, blog_writer],
+    tasks=[research_task, write_task],
+    process=Process.sequential,
+    memory=True,
+    cache = True,
+    max_rpm=100,
+    share_crew= True
+)
+
+#starting the task execution process with enhacned feedback 
+result = crew.kickoff(inputs={"topic": "AI VS ML VS DL VS Data Science" })
+print(result)
